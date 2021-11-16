d74cc9e426c2c1e167d0c994469529fc47e70e59
anonymous anonymous <rahul.jayaprakash@kellogg.northwestern.edu>
2021-11-16 18:03:56 +0000

    Whitelist columns in Cast for ActiveAdmin

--- app/admin/casts.rb
+++ app/admin/casts.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Cast do
 
+ permit_params :character_id, :movie_id, :actors_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
