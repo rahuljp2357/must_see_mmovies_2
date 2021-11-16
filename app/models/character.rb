13aba4272ce3c0984a11bfa904c6566850c9f27b
anonymous anonymous <rahul.jayaprakash@kellogg.northwestern.edu>
2021-11-16 18:03:41 +0000

    Generate characters resource with ideas:scaffold

---
+++ app/models/character.rb
@@ -0,0 +1,14 @@
+class Character < ApplicationRecord
+  # Direct associations
+
+  # Indirect associations
+
+  # Validations
+
+  # Scopes
+
+  def to_s
+    character_name
+  end
+
+end
---
+++ app/views/characters/_character.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! character, :id, :character_name, :movie_id, :created_at, :updated_at
+json.url character_url(character, format: :json)
