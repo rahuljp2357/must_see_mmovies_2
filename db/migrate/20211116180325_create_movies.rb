6acd95ed7a30ff21a9a16b8765639454b2087aaf
anonymous anonymous <rahul.jayaprakash@kellogg.northwestern.edu>
2021-11-16 18:03:26 +0000

    Generate movies resource with ideas:scaffold

---
+++ db/migrate/20211116180325_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.date :year
+      t.string :duration
+      t.string :description
+      t.integer :director_id
+
+      t.timestamps
+    end
+  end
+end
